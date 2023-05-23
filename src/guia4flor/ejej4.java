9a1426b9e53528689e77c308de24e4b6e1626d18
floslobs <fan1_avril@hotmail.com>
2023-05-23 10:22:39 -0300

    se realiza ejercicio 4 si el numero es primo

--- src/guia4flor/ejej4.java
+++ src/guia4flor/ejej4.java
@@ -5,6 +5,8 @@
  */
 package guia4flor;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ARG
@@ -22,6 +24,34 @@ public class ejej4 {
 Un número primo es aquel que solo puede dividirse entre 1 y sí mismo. Por ejemplo: 25 no es
         primo, ya que 25 es divisible entre 5, sin embargo, 17 si es primo.
 */
+
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+
+        System.out.println("ingrese un numero ");
+        int num = sc.nextInt();
+        primo(num);
+
     }
-    
+
+    public static boolean primo(int num) {
+
+        // numero primo es divisible por 1 o por si mismo
+        int aux = 0; // auxiliar que ayuda a contar los ceros
+
+        for (int i = 1; i <= num; i++) {
+
+            if (num % i == 0) {
+                aux ++;    //va sumando los ceros, si la cantidad de 0 es 2 quieres decir q es primo
+
+            }
+            if (aux > 2) { // si la cantidad de ceros es mas de dos no es primo
+                break;
+                
+            }
+
+        }
+        return aux <= 2;
+
+    }
+
 }
